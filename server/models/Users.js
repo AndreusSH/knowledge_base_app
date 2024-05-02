6567ba06c8091d0dc99543cdfaea9caf4f1e2a21
AndreusSH <andreamammoliti@yahoo.it>
2024-05-02 12:15:00 +0200

    schema for users

--- server/models/Users.js
+++ server/models/Users.js
@@ -4,5 +4,13 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    family_name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
     }
 })
